32fbdd42fca87a0333eae65f5666040b0af4aa0a
ALEXEY SMIRNOV <msc.smirnov.am@gmail.com>
2016-05-30 17:21:49 +0300

    use requests and new alazysing algorithm

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
 # cucm_srv_perfmon_api
-This script written in python (2.7.9) queries CUCM (Cisco Unified Communicaion Manager) Serviceability API for a registration status of phones and other devices controled by it. 
+This script written in python queries CUCM (Cisco Unified Communicaion Manager) Serviceability API for a registration status of phones and other devices controled by it.
 More at https://developer.cisco.com/site/sxml/discover/overview/perfmon/
 
 For script to work:
 
-1) Add approriate user for Serviceabibty API: add him to AXLUSERS, CCM Admin Users, CCM Super Users groups
+1) Run SOAP services and add an AXL user as described here https://developer.cisco.com/site/sxml/learn/getting-started/risport/index.gsp#service-activation
 
-2) Change IP address of your CUCM publisher (do not forget to activate appropriate services on it)
+2) Clone Install requirements
+git clone https://github.com/smirnov-am/cucm_srv_perfmon_api
+pip install -r requirements
 
-3) Select model types you are interested in 
+3) Run script with credentials from p.1
+chmod +x cmdevices.py
+./cmdevices.py -c 192.168.0.1 -u axl_admin -p *****
 
-4) Change output of the script if needed
--- cmdevices.py
+++ cmdevices.py
@@ -1,70 +1,124 @@
-import sys
-import ssl
-import urllib2
-from suds.xsd.doctor import Import
-from suds.xsd.doctor import ImportDoctor
-from suds.client import Client
-from suds.transport.https import HttpAuthenticated
-from email.mime.text import MIMEText
-from subprocess import Popen, PIPE
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Script for querying Cisco RisPort Serviceability API for device registration status.
+Api descripbed at https://developer.cisco.com/site/sxml/documents/api-reference/risport/
 
-# Defining some variable
-CMServer 	= '192.168.0.1' #IP of CUCM publisher
-port 	= '8443'
-user 		= 'srv_api_user' # Do not forget to create this user and add him to appropriate groups
-passwd 		= '**********';
-wsdl = 'https://'+ CMServer + ':' + port +'/realtimeservice/services/RisPort?wsdl'
-location = 'https://' + CMServer + ':' + port + '/realtimeservice/services/RisPort'
+CLI Args:
+    "-c", "--cucm" CUCM node IP
+    "-u", "--user" AXL username
+    "-p", "--password" AXL user password
+    "-m", "--models" models (int values separated by space - consult models.txt for mappings to names)
+    (i.e -m 626 682 will monitor SX10 and SX20 statuses)
+"""
 
-# Here are the models of cmdevices registration status we are querying. For the full list of model ids see models.txt
-models=['626', '682', '431']
+import argparse
+import requests
+import xml.etree.ElementTree as ET
+from collections import defaultdict
+from time import sleep
+from datetime import datetime
+from xml.sax.saxutils import escape
+from copy import deepcopy
+import re
+from itertools import izip_longest
 
-# Repairing xml schema
-tns = 'http://schemas.cisco.com/ast/soap/'
-imp = Import('http://schemas.xmlsoap.org/soap/encoding/', 'http://schemas.xmlsoap.org/soap/encoding/')
-imp.filter.add(tns)
+requests.packages.urllib3.disable_warnings()
 
-# As soon as we are using https for access to CUCM we need an appropriate ssl context. 
-# Following code works with ssl module from python 2.7.9.
-ssl_def_context = ssl.create_default_context()
-ssl_def_context.check_hostname = False
-ssl_def_context.verify_mode = ssl.CERT_NONE
-
-# Main action starts here
-layout=[]
-for model in models:
-    t0 = HttpAuthenticated(username=user, password=passwd)
-    t0.handler=urllib2.HTTPBasicAuthHandler(t0.pm)
-    t1=urllib2.HTTPSHandler(context=ssl_def_context)
-    t0.urlopener = urllib2.build_opener(t0.handler,t1)
-    client=Client(wsdl,location=location, plugins=[ImportDoctor(imp)], transport=t0)
-    result = client.service.SelectCmDevice('',{'SelectBy':'Name', 'Class':'Phone', 'Model':model})
-    for node in result['SelectCmDeviceResult']['CmNodes']:
-        for device in node['CmDevices']:
-            layout.append({ 'Name':str(device['Name']),
-                            'Description': unicode(device['Description']),
-                            'IP': str(device['IpAddress']),
-                            'Status':str(device['Status']),
-                            'Model':str(device['Model']),
-                            'Registrar':str(node['Name'])
-                                   })
+# this is a template for SelectCmDevice request body
+select_cm_device_req = """
+<SOAP-ENV:Envelope xmlns:ns3="http://www.w3.org/2001/XMLSchema"
+xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/"
+xmlns:ns0="http://schemas.xmlsoap.org/soap/encoding/"
+xmlns:ns1="http://schemas.cisco.com/ast/soap/"
+xmlns:ns2="http://schemas.xmlsoap.org/soap/envelope/"
+xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/"
+SOAP-ENV:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
+<SOAP-ENV:Header/>
+<ns2:Body>
+<ns1:SelectCmDevice>
+<StateInfo xsi:type="ns3:string">{}</StateInfo>
+<CmSelectionCriteria xsi:type="ns1:CmSelectionCriteria">
+<ns1:MaxReturnedDevices xsi:type="xsd:unsignedInt">10000</ns1:MaxReturnedDevices>
+<ns1:Class xsi:type="ns3:string">Phone</ns1:Class>
+<ns1:Model xsi:type="ns3:unsignedInt">{}</ns1:Model>
+<ns1:SelectBy xsi:type="ns3:string">Name</ns1:SelectBy>
+</CmSelectionCriteria></ns1:SelectCmDevice>
+</ns2:Body>
+</SOAP-ENV:Envelope>"""
 
 
-# Strangely enough but CUCM (10.5.2 in my case) shows device status as Unregistered if it's unregistered from one node but registered on another
-# Following code will search for such split status and reduce layout 								   
-layout_reduce=[]
-for i in range(len(layout)):
-    for j in range(i+1,len(layout)):
-        if (layout[i]['Name']==layout[j]['Name']) and (layout[i]['Status']!=layout[j]['Status']) and (layout[i]['Status']=='Registered'  and layout[j]['Status']=='UnRegistered'):
-            layout[j]['Status']='split'
-        if (layout[i]['Name']==layout[j]['Name']) and (layout[i]['Status']!=layout[j]['Status']) and (layout[i]['Status']=='UnRegistered'  and layout[j]['Status']=='Registered'):
-            layout[i]['Status']='split'
-for i in layout:
-    if i['Status']!='split':
-        layout_reduce.append(i)
+def compare_outputs(a, b):
+    """Compare previous and current result of a SelectCmDevice query from CUCM
+    Now it doesn't support StateInfo and used to compare consequtive queries
+    :param a: previous query
+    :param b: current query
+    :return: list of strings describing changes
+    """
+    a_keys = set(a.keys())
+    b_keys = set(b.keys())
+    keys_added = b_keys - a_keys
+    keys_deleted = a_keys - b_keys
+    _common = a_keys & b_keys
+    common_eq = set(k for k in _common if sorted(a[k]) == sorted(b[k]))
+    common_neq = _common - common_eq
+    added = ['{}  added'.format(x) for x, y in b.items() if x in keys_added]
+    deleted = ['{} deleted'.format(x) for x, y in a.items() if x in keys_deleted]
+    changed = []
+    for i in common_neq:
+        for pair in izip_longest(sorted(a[i]), sorted(b[i]), fillvalue=dict((k, 'NA') for k in ('Status', 'IP'))):
+            for key in ('Status', 'IP'):
+                if pair[0][key] != pair[1][key]:
+                    changed.append('{} changed {} to {} on node {}'.format(i, key, pair[1][key],pair[1]['node']))
+    return added + deleted + changed
 
 
-# Here we are going to print device status
-for i in layout_reduce:
-    print "Status of %s (%s) with IP %s is %s  at server %s" %(i['Name'],i['Description'],i['IP'],i['Status'],i['Registrar'])
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--cucm", help="CUCM node IP", required=True)
+    parser.add_argument("-u", "--user", help="AXL username", required=True)
+    parser.add_argument("-p", "--password", help="AXL user password", required=True)
+    parser.add_argument("-m", "--models", help="models", default='682 626')
+    args = parser.parse_args()
+    headers = {'SOAPAction': '"http://schemas.cisco.com/ast/soap/action/#RisPort#SelectCmDevice"'}
+    state_info = ''
+    temp = defaultdict(list)
+    models = {}
+    models_re = re.compile('(\d+) (.+)')
+    with open('models.txt', 'r') as f:
+        for model in f:
+            models[models_re.split(model)[1]] = models_re.split(model)[2]
+    models_required = args.models.split()
+    try:
+        while True:
+            all_devices = defaultdict(list)
+            for model in models_required:
+                response = requests.post('https://{}:8443/realtimeservice/services/RisPort'.format(args.cucm),
+                                         data=select_cm_device_req.format(state_info, model),
+                                         auth=requests.auth.HTTPBasicAuth(args.user, args.password),
+                                         headers = headers,
+                                         verify=False,
+                                         timeout=5)
 
+                xml = ET.fromstring(response.text)
+                for node in xml.iter('item'):
+                    if 'ns1:CmNode' in node.attrib.values():
+                        node_name = node.find('Name').text
+                        for j in node.find('CmDevices').findall('item'):
+                            all_devices[j.find('Name').text].append({'IP': j.find('IpAddress').text,
+                                                                     'node': node_name,
+                                                                     'Model': models[j.find('Model').text],
+                                                                     'Description': j.find('Description').text,
+                                                                     'Status': j.find('Status').text})
+
+
+                # SelectCmdevices supports Stateinfo string to be sent with subsequent requests. Try this!!
+                #for j in xml.iter('StateInfo'):
+                #    state_info = escape(j.text)
+                sleep(1)
+            for s in compare_outputs(temp, all_devices):
+                print '{} : {}'.format(datetime.now(), s)
+            temp = deepcopy(all_devices)
+            sleep(15)
+    except (SystemExit, KeyboardInterrupt):
+        print "Exiting"
