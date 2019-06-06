c573c59c866ec62e567b5c1d9442a469c9cc616b
Prasanna Saraf <prasanas@thoughtworks.com>
2019-06-06 20:00:58 +0530

    Prasanna| Working solutions

--- cmdevices.py
+++ cmdevices.py
@@ -37,43 +37,13 @@ SOAP-ENV:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
 <SOAP-ENV:Header/>
 <ns2:Body>
 <ns1:SelectCmDevice>
-<StateInfo xsi:type="ns3:string">{}</StateInfo>
 <CmSelectionCriteria xsi:type="ns1:CmSelectionCriteria">
 <ns1:MaxReturnedDevices xsi:type="xsd:unsignedInt">10000</ns1:MaxReturnedDevices>
-<ns1:Class xsi:type="ns3:string">Phone</ns1:Class>
-<ns1:Model xsi:type="ns3:unsignedInt">{}</ns1:Model>
+<ns1:Class xsi:type="ns3:string">SIP Trunk</ns1:Class>
 <ns1:SelectBy xsi:type="ns3:string">Name</ns1:SelectBy>
 </CmSelectionCriteria></ns1:SelectCmDevice>
 </ns2:Body>
 </SOAP-ENV:Envelope>"""
-
-
-def compare_outputs(a, b):
-    """Compare previous and current result of a SelectCmDevice query from CUCM
-    Now it doesn't support StateInfo and used to compare consequtive queries
-    :param a: previous query
-    :param b: current query
-    :return: list of strings describing changes
-    """
-    a_keys = set(a.keys())
-    b_keys = set(b.keys())
-    keys_added = b_keys - a_keys
-    keys_deleted = a_keys - b_keys
-    _common = a_keys & b_keys
-    common_eq = set(k for k in _common if sorted(a[k]) == sorted(b[k]))
-    common_neq = _common - common_eq
-    added = ['{}  added'.format(x) for x, y in b.items() if x in keys_added]
-    deleted = ['{} deleted'.format(x)
-               for x, y in a.items() if x in keys_deleted]
-    changed = []
-    for i in common_neq:
-        for pair in izip_longest(sorted(a[i]), sorted(b[i]), fillvalue=dict((k, 'NA') for k in ('Status', 'IP'))):
-            for key in ('Status', 'IP'):
-                if pair[0][key] != pair[1][key]:
-                    changed.append('{} changed {} to {} on node {}'.format(
-                        i, key, pair[1][key], pair[1]['node']))
-    return added + deleted + changed
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -81,48 +51,12 @@ if __name__ == '__main__':
     parser.add_argument("-u", "--user", help="AXL username", required=True)
     parser.add_argument("-p", "--password",
                         help="AXL user password", required=True)
-    parser.add_argument("-m", "--models", help="models", default='682 626')
     args = parser.parse_args()
     headers = {
         'SOAPAction': '"http://schemas.cisco.com/ast/soap/action/#RisPort#SelectCmDevice"'}
-    state_info = ''
-    temp = defaultdict(list)
-    models = {}
-    models_re = re.compile('(\d+) (.+)')
-    with open('models.txt', 'r') as f:
-        for model in f:
-            models[models_re.split(model)[1]] = models_re.split(model)[2]
-    models_required = args.models.split()
     try:
-        all_devices = defaultdict(list)
-        for model in models_required:
-            response = requests.post('https://{}:8443/realtimeservice/services/RisPort'.format(args.cucm),
-                                     data=select_cm_device_req.format(
-                                         state_info, model),
-                                     auth=requests.auth.HTTPBasicAuth(
-                                         args.user, args.password),
-                                     headers=headers,
-                                     verify=False,
-                                     timeout=5)
-
-            xml = ET.fromstring(response.text)
-            for node in xml.iter('item'):
-                if 'ns1:CmNode' in node.attrib.values():
-                    node_name = node.find('Name').text
-                    for j in node.find('CmDevices').findall('item'):
-                        all_devices[j.find('Name').text].append({'IP': j.find('IpAddress').text,
-                                                                 'node': node_name,
-                                                                 'Model': models[j.find('Model').text],
-                                                                 'Description': j.find('Description').text,
-                                                                 'Status': j.find('Status').text})
-
-            # SelectCmdevices supports Stateinfo string to be sent with subsequent requests. Try this!!
-            # for j in xml.iter('StateInfo'):
-            #    state_info = escape(j.text)
-            sleep(1)
-        for s in compare_outputs(temp, all_devices):
-            print '{} : {}'.format(datetime.now(), s)
-        temp = deepcopy(all_devices)
-        sleep(15)
+        response = requests.post('https://{}:8443/realtimeservice/services/RisPort'.format(args.cucm),data=select_cm_device_req,auth=requests.auth.HTTPBasicAuth(args.user, args.password),headers=headers,verify=False,timeout=5)
+        print(response)
+        print(response.text)
     except (SystemExit, KeyboardInterrupt):
         print "Exiting"
